4c5a096eb9235fe590b2d9b559dc864cc06927c0
jai1726 <jagadeeshdende@gmail.com>
2024-11-17 23:52:55 +0530

    Inclusion of routers  -auth  -user  -admin

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const cors=require('cors');
 const dotenv = require('dotenv');
 const connectDB=require('./db');
 const router = require('./routes/User.Routes.js');
-
-
+const adminRouter=require('./routes/Admin.Routes.js');
+const authRouter=require('./routes/Auth.Routes.js');
 dotenv.config();
 
 const PORT=process.env.PORT||5500;
-const app=express();
+const app=express();  
 app.use(cors());
 app.use(express.json());
 
-connectDB();
-app.use("/api/user", router);
+connectDB();   // calling function to connect Database
+
+
+app.use("/api/user", router);   //user endpoint 
+app.use("/api/admin",adminRouter);   //admin endpoint
+app.use("/api/auth",authRouter);   //Auth  endpoint
 
 app.use('/',(req,res)=>{
     res.json("get working");
